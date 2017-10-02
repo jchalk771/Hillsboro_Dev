f9f2e1cfed77962d2c1d10a263563885c7bde017
Jeanne Chalk <jchalk@accela.com>
2017-10-02 12:31:51 -0400

    from UAT - tested

--- INCLUDES_CUSTOM/adjustBldgValFees.js
+++ INCLUDES_CUSTOM/adjustBldgValFees.js
@@ -3,10 +3,10 @@ function adjustBldgValFees(recordStatus) {
 		//Existing fee custom functions also contain check for Recalc Fees = Yes.
 		//This function is to augment that in the event that Recalc Fees is not returned to No after an adjustment of scope.
 		var adjustStatus = true;
-	
+
 		//Open and C of O Issued are being included as there are older records in the system within these statuses.
 		var recordStatusToExclude = ["About to Expire", "Closed", "Deferred", "Expired", "Finaled", "Withdrawn", "Issued", "Ready to Issue", "Void", "C of O Pending", "Open"];
-		
+
 		for (thisStatus in recordStatusToExclude) {
 			if (recordStatusToExclude[thisStatus] == recordStatus) {
 				logDebug("Not Recalculating because the status: " + recordStatus + " is excluded in the adjustBldgValFees function");
@@ -16,7 +16,7 @@ function adjustBldgValFees(recordStatus) {
 			}
 		}
 		return adjustStatus;
-		
+
 	} catch (err) {
 		logDebug("A JavaScript error has occurred in custom function adjustBldgValFees: " + err.message + "In line number: " + err.lineNumber);
 	}
