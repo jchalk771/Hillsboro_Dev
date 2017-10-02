5e49322f93ab1421eed632f500e0c6d4e83b63f7
Jeanne Chalk <jchalk@accela.com>
2017-10-02 12:39:13 -0400

    Optional utility function

--- INCLUDES_CUSTOM/getFeeCodeMax.js
+++ INCLUDES_CUSTOM/getFeeCodeMax.js
@@ -1,16 +1,16 @@
-function getFeeCodeMax(pFeeSched, pFeeCode) {
+function getFeeCodeMax(fsched, feeCode) {
 	try {
 		var maxValue = false;
-		var arrFeesResult = aa.finance.getFeeItemList(null, pFeeSched, null);
+		var arrFeesResult = aa.finance.getFeeItemList(null, fsched, null);
 		if (!arrFeesResult.getSuccess()) {
-			logDebug("Unable to retrieve fee schedule: " + pFeeSched);
+			logDebug("Unable to retrieve fee schedule: " + fsched);
 			return false;
 		} else {
 			var arrFees = arrFeesResult.getOutput();
 			for (xx in arrFees) {
 				thisFee = arrFees[xx];
 				var fCode = thisFee.getFeeCod();
-				if (fCode.equals(pFeeCode)) {
+				if (fCode.equals(feeCode)) {
 					maxValue = roundNumber(thisFee.getMaxFee(), 2).toFixed(2);
 					break;
 				}
@@ -20,7 +20,7 @@ function getFeeCodeMax(pFeeSched, pFeeCode) {
 				logDebug("Max value for fee code " + feeCode + " found: " + maxValue);
 				return maxValue;
 			} else {
-				logDebug("Fee code " + pFeeCode + " has no max value set");
+				logDebug("Fee code " + feeCode + " has no max value set");
 				return false;
 			}
 		}
