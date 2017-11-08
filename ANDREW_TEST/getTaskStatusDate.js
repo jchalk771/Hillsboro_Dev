0ef852115d7f8dfec1852144287906fda4a353f7
fragrom <fragrom@gmail.com>
2017-11-08 15:23:14 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getTaskStatusDate.js
@@ -0,0 +1,33 @@
+function getTaskStatusDate(wfstr) // optional process name, optional capID
+{
+	var useProcess = false;
+	var processName = "";
+	var itemCap = capId;
+	if (arguments.length > 1) {
+		processName = arguments[1]; // subprocess
+		if (processName != null)
+			useProcess = true;
+	}
+	if (arguments.length > 2) {
+		itemCap = arguments[2];
+	}
+
+	var workflowResult = aa.workflow.getTasks(itemCap);
+	if (workflowResult.getSuccess())
+		var wfObj = workflowResult.getOutput();
+	else {
+		logMessage("**ERROR: Failed to get workflow object: " + workflowResult.getErrorMessage());
+		return null;
+	}
+
+	for (i in wfObj) {
+		fTask = wfObj[i];
+		if (fTask.getTaskDescription().toUpperCase().equals(wfstr.toUpperCase()) && (!useProcess || fTask.getProcessCode().equals(processName))) {
+			if (fTask.getStatusDate()) {
+				return ("" + (fTask.getStatusDate().getMonth() + 1) + "/" + fTask.getStatusDate().getDate() + "/" + (fTask.getStatusDate().getYear() + 1900));
+			}
+		}
+	}
+	return null;
+}
+
