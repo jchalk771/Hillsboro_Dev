0b2cc0c3387cb5b0c873470879933a924870c9c4
fragrom <fragrom@gmail.com>
2017-11-07 16:51:16 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getReqInspOnRecord.js
@@ -0,0 +1,25 @@
+getReqInspOnRecord()
+ {
+
+	var retValue = "";
+	var inspResultObj = aa.inspection.getInspections(capId);
+	if (inspResultObj.getSuccess()) {
+		var inspList = inspResultObj.getOutput();
+		if (inspList != null) {
+			for (xx in inspList) {
+				var thisInsp = inspList[xx];
+				thisInspType = thisInsp.getInspectionType();
+				inspModel = thisInsp.getInspection();
+				if (inspModel != null) {
+					activityModel = inspModel.getActivity();
+					if (activityModel.getRequiredInspection() == "Y") {
+						if (retValue != "")
+							retValue += ",";
+						retValue += thisInspType;
+					}
+				}
+			}
+		}
+	}
+	return retValue;
+}
