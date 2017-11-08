ce0e106ae9e7abc8e6287a2f5a43812fcf93465c
fragrom <fragrom@gmail.com>
2017-11-07 16:52:24 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getACADeepLinkParam4Notification.js
@@ -0,0 +1,9 @@
+getACADeepLinkParam4Notification(params, acaUrl, pAppType, pAppTypeAlias, module)
+ {
+	// pass in a hashtable and it will add the additional parameters to the table
+
+	addParameter(params, "$$acaDeepLinkUrl$$", getDeepLinkUrl(acaUrl, pAppType, module));
+	addParameter(params, "$$acaDeepLinkAppTypeAlias$$", pAppTypeAlias);
+
+	return params;
+}
