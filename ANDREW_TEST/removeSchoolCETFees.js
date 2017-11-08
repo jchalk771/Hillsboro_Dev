66d5d03c9f48ff3de18ecec5ec4dee91f4b1c5bf
fragrom <fragrom@gmail.com>
2017-11-07 16:52:13 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/removeSchoolCETFees.js
@@ -0,0 +1,16 @@
+removeSchoolCETFees()
+ {
+
+	//var schoolFeeArray = new Array("B_STR_060","B_STR_065","B_STR_090","B_STR_095","B_STR_085","B_STR_070","B_STR_115","B_STR_110");
+	var schoolFeeArray = lookup("School_Fee_Code_Lookup", "AllCombinedFees").split("|");
+
+	for (feeItem in schoolFeeArray) {
+
+		var feeCode = schoolFeeArray[feeItem];
+
+		if (feeExists(feeCode)) {
+			reduceFeeAmtToZero(feeCode, "B_STR", "STANDARD");
+		}
+	}
+
+}
