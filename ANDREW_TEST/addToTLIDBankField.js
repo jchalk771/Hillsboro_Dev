40b3380b40c093025e6f0f98e6e785e9ce6baaf6
fragrom <fragrom@gmail.com>
2017-11-07 16:51:18 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/addToTLIDBankField.js
@@ -0,0 +1,12 @@
+addToTLIDBankField(fieldName, amtToAdd)
+ {
+
+	bankValue = getTLIDBankField(fieldName);
+	if (bankValue == null || bankValue == undefined || bankValue == "")
+		bankValue = 0;
+	else
+		bankValue = parseFloat(bankValue);
+
+	if (amtToAdd != null && amtToAdd != "")
+		updateTLIDBankField(fieldName, roundNumber(bankValue + parseFloat(amtToAdd), 2));
+}
