24ae4d0a8da857b8c6de5b1cf133984c826cf934
fragrom <fragrom@gmail.com>
2017-11-07 16:50:56 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getFeeDefByDesc.js
@@ -0,0 +1,25 @@
+getFeeDefByDesc(fsched, feeDesc)
+ {
+
+	var arrFeesResult = aa.finance.getFeeItemList(null, fsched, null);
+	if (arrFeesResult.getSuccess()) {
+		var arrFees = arrFeesResult.getOutput();
+		for (xx in arrFees) {
+			var fDesc = arrFees[xx].getFeeDes();
+			if (fDesc.equals(feeDesc)) {
+				var f = new FeeDef();
+				f.feeCode = arrFees[xx].getFeeCod();
+				f.feeDesc = fDesc;
+				f.formula = arrFees[xx].getFormula();
+				f.calcProc = arrFees[xx].getCalProc();
+				var rft = arrFees[xx].getrFreeItem();
+				f.comments = rft.getComments();
+				return f;
+			}
+
+		} // for xx
+	} else {
+		logDebug("Error getting fee schedule " + arrFeesResult.getErrorMessage());
+		return null;
+	}
+}
