e4754132dc71af3271252222e3268cc41e9e1bab
fragrom <fragrom@gmail.com>
2017-11-07 16:51:00 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/sumASITColumn.js
@@ -0,0 +1,39 @@
+sumASITColumn(tObj, cNameToSum)
+ {
+	// optional params = cFilterType, cNameFilter, cValueFilter
+	var retValue = 0;
+	if (tObj) {
+		if (arguments.length == 2) { // no filters
+			for (var ea in tObj) {
+				var row = tObj[ea];
+				var colValue = row[cNameToSum].fieldValue;
+				if (!isNaN(parseFloat(colValue)))
+					retValue += parseFloat(colValue);
+			}
+			return retValue;
+		}
+		if (arguments.length == 5) {
+			filterType = arguments[2];
+			cNameFilter = arguments[3];
+			cValueFilter = arguments[4];
+			for (var ea in tObj) {
+				var row = tObj[ea];
+				var colValue = row[cNameToSum].fieldValue;
+				var colFilter = row[cNameFilter].fieldValue;
+				if (filterType == "INCLUDE") {
+					if (colFilter == cValueFilter) {
+						if (!isNaN(parseFloat(colValue)))
+							retValue += parseFloat(colValue);
+					}
+				}
+				if (filterType == "EXCLUDE") {
+					if (colFilter != cValueFilter) {
+						if (!isNaN(parseFloat(colValue)))
+							retValue += parseFloat(colValue);
+					}
+				}
+			}
+		}
+	}
+	return retValue;
+}
