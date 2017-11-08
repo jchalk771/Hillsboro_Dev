4265741715127945abf8d1393fce35a0aad80079
fragrom <fragrom@gmail.com>
2017-11-07 16:51:33 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/updateASSURANCESasit.js
@@ -0,0 +1,18 @@
+updateASSURANCESasit(wfDate)
+ {
+	tObj = loadASITable("ASSURANCES");
+	newTable = new Array();
+	var reloadTable = false;
+	for (row in tObj) {
+		reloadTable = true;
+		thisRow = tObj[row];
+		if (thisRow["Start"].fieldValue == null || thisRow["Start"].fieldValue == "") {
+			thisRow["Start"].fieldValue = wfDate;
+		}
+		newTable.push(thisRow);
+	}
+	if (reloadTable) {
+		removeASITable("ASSURANCES");
+		addASITable("ASSURANCES", newTable);
+	}
+}
