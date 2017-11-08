127222061e2636af5563d8b61d6541b2d1776535
fragrom <fragrom@gmail.com>
2017-11-08 15:23:00 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getPrimaryParcelNum.js
@@ -0,0 +1,22 @@
+function getPrimaryParcelNum() {
+	parcelToUse = null;
+	parcelNum = null;
+	var capParcelResult = aa.parcel.getParcelandAttribute(capId, null);
+	if (capParcelResult.getSuccess()) {
+		var Parcels = capParcelResult.getOutput().toArray();
+		for (zz in Parcels) {
+			thisParcel = Parcels[zz];
+			if (thisParcel.getPrimaryParcelFlag() == "Y") {
+				parcelToUse = thisParcel;
+				break;
+			}
+		}
+		if (parcelToUse == null && Parcels.length > 0)
+			parcelToUse = Parcels[0];
+	}
+	if (parcelToUse != null) {
+		parcelNum = parcelToUse.getParcelNumber();
+	}
+	return parcelNum;
+}
+
