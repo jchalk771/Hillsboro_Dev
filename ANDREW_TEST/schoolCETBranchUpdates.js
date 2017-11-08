a26be8eadaf4db1b153d8fc9652a1a679b4cb4ea
fragrom <fragrom@gmail.com>
2017-11-07 16:52:16 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/schoolCETBranchUpdates.js
@@ -0,0 +1,20 @@
+schoolCETBranchUpdates()
+ {
+
+	var schoolDistrict = AInfo["School District"];
+	var resSqFt = AInfo["Residential Living Area SqFt"];
+	var comSqFt = AInfo["Non Residential SqFt"];
+	var sCETRefused = AInfo["School CET Refused"];
+	var sCETExempt = AInfo["School CET Exempt"];
+
+	assessSchoolCETFees(schoolDistrict, fileDateObj, resSqFt, comSqFt);
+
+	if (comSqFt == 0 && resSqFt == 0)
+		removeSchoolCETFees();
+
+	removeIncorrectDistrictFees(schoolDistrict);
+
+	if (sCETRefused == "Yes" || sCETExempt == "Yes")
+		waiveSchoolCETFees(schoolDistrict);
+
+}
