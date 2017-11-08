c45b4c4bf0eec49e9096e03ab02e02baa3022e32
fragrom <fragrom@gmail.com>
2017-11-07 16:52:34 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/sohiTSDCCcheck.js
@@ -0,0 +1,12 @@
+sohiTSDCCcheck()
+ {
+	var J;
+	var H;
+	J = "false";
+	J = doesASITableExistAndHaveSomeValue("SOHI TSDC INFORMATION");
+	if (J == "true") {
+		updateSoHiTSDCFee();
+	} else {
+		return;
+	}
+}
