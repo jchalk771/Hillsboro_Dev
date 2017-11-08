e317261a303f86f6d44276bf5227f9719cb1a348
fragrom <fragrom@gmail.com>
2017-11-07 16:51:28 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/areAllChildrenFinal.js
@@ -0,0 +1,12 @@
+areAllChildrenFinal()
+ {
+	childList = getChildren("*/*/*/*");
+	if (childList == null)
+		return true;
+	for (xx in childList) {
+		if (aa.cap.getCap(childList[xx]).getOutput().getCapStatus() != "Finaled") {
+			return false;
+		}
+	}
+	return true;
+}
