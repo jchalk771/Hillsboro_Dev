5deae57657ab5e13ad988661a85acb6fd3c384fd
fragrom <fragrom@gmail.com>
2017-11-07 16:52:39 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/FeeDef.js
@@ -0,0 +1,17 @@
+FeeDef() { // Fee Definition object
+	try {
+
+		this.formula = null;
+		this.feeUnit = null;
+		this.feeDesc = null;
+		this.feeCode = null;
+		this.comments = null;
+		this.calcProc = null;
+		this.feeMax = null;
+		this.feeMin = null;
+
+	} catch (err) {
+		logDebug("A JavaScript error has occurred in custom function FeeDef-INCLUDES_CUSTOM: " + err.message + "In line number: " + err.lineNumber);
+	}
+}
+
