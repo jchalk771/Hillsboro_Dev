243b36093d3bd40916e379dafed911db9c05611d
fragrom <fragrom@gmail.com>
2017-11-07 16:51:35 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/editJobValue.js
@@ -0,0 +1,14 @@
+editJobValue(newJobValue)
+ {
+	var valobj = aa.finance.getContractorSuppliedValuation(capId, null).getOutput();
+	if (valobj.length) {
+		estValue = valobj[0].getEstimatedValue();
+		calcValue = valobj[0].getCalculatedValue();
+		feeFactor = valobj[0].getbValuatn().getFeeFactorFlag();
+		editResult = aa.finance.editBValuatnValue(capId, valobj[0].getValuationPeriod(), newJobValue, feeFactor);
+		if (editResult.getSuccess())
+			logDebug("Modified job value");
+		else
+			logDebug("Error editing job value " + editResult.getErrorMessage());
+	}
+}
