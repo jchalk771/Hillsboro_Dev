7d9d8243b0f6690bceb724edeb5f8d372242229c
fragrom <fragrom@gmail.com>
2017-11-07 16:52:36 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/engTDTcheck.js
@@ -0,0 +1,12 @@
+engTDTcheck()
+ {
+	var J;
+	var H;
+	J = "false";
+	J = doesASITableExistAndHaveSomeValue("TDT SDC INFORMATION");
+	if (J == "true") {
+		updateTDTFee();
+	} else {
+		return;
+	}
+}
