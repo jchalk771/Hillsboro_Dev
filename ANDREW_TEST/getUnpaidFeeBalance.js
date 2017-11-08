6b741218e5795c0caae734c84aa4e636dea83fb8
fragrom <fragrom@gmail.com>
2017-11-08 15:23:23 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getUnpaidFeeBalance.js
@@ -0,0 +1,12 @@
+function getUnpaidFeeBalance() {
+
+	totFee = getFeeTotal("NEW", "INVOICED");
+	totPay = getPaymentTotal();
+	b = totFee - totPay;
+	if (b > 0.1)
+		return b;
+	else
+		return 0;
+
+}
+
