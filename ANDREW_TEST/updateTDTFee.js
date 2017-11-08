2add0fbeb9b5a0b88394de19ffec3cbd33e0e27b
fragrom <fragrom@gmail.com>
2017-11-07 16:51:13 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/updateTDTFee.js
@@ -0,0 +1,24 @@
+updateTDTFee()
+ {
+
+	feeCode = "B_TDT_001";
+	tdtSDCTable = loadASITable("TDT SDC INFORMATION");
+	if (tdtSDCTable == undefined || tdtSDCTable == null || tdtSDCTable.length == 0) {
+		logDebug("No TDT SDC information in ASIT");
+		if (feeExists(feeCode))
+			reduceFeeAmtToZero(feeCode, "B_TDT", "STANDARD"); // gff 4-30
+		return;
+	}
+
+	feeSum = sumASITColumn(tdtSDCTable, "Line Item Fee Total");
+	if (feeSum > 0) {
+		//if (feeExists(feeCode) && (feeSum != getFeeQty(feeCode))) {
+		//	voidRemoveFees(feeCode);
+		//}
+		updateFee(feeCode, "B_TDT", "STANDARD", feeSum, "N");
+	} else {
+		if (feeExists(feeCode))
+			//voidRemoveFees(feeCode);
+			reduceFeeAmtToZero(feeCode, "B_TDT", "STANDARD"); // gff 4-30
+	}
+}
