b13c33f98690e6ca9b991b97620485d5fdce4683
fragrom <fragrom@gmail.com>
2017-11-07 16:51:04 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getAddressInALine.js
@@ -0,0 +1,52 @@
+getAddressInALine()
+ {
+
+	var capAddrResult = aa.address.getAddressByCapId(capId);
+	var addressToUse = null;
+	var strAddress = "";
+
+	if (capAddrResult.getSuccess()) {
+		var addresses = capAddrResult.getOutput();
+		if (addresses) {
+			for (zz in addresses) {
+				capAddress = addresses[zz];
+				if (capAddress.getPrimaryFlag() && capAddress.getPrimaryFlag().equals("Y"))
+					addressToUse = capAddress;
+			}
+			if (addressToUse == null)
+				addressToUse = addresses[0];
+
+			if (addressToUse) {
+				var addPart = addressToUse.getHouseNumberStart();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getStreetDirection();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getStreetName();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getStreetSuffix();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getUnitType();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getUnitStart();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getCity();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart + ",";
+				var addPart = addressToUse.getState();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				var addPart = addressToUse.getZip();
+				if (addPart && addPart != "")
+					strAddress += " " + addPart;
+				return trimString(strAddress);
+			}
+		}
+	}
+	return null;
+}
