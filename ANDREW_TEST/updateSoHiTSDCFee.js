1c21bdbd41ae4b3c8a032372df723ce1685bb1a3
fragrom <fragrom@gmail.com>
2017-11-08 15:23:57 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/updateSoHiTSDCFee.js
@@ -0,0 +1,30 @@
+function updateSoHiTSDCFee() {
+	logDebug("I'm executing updateSoHiTSDCFEE");
+	feeCode = "B_TDT_006";
+	tdtSDCTable = loadASITable("SOHI TSDC INFORMATION");
+	if (tdtSDCTable == undefined || tdtSDCTable == null || tdtSDCTable.length == 0) {
+		logDebug("No South Hillsboro TSDC information in ASIT");
+		if (feeExists(feeCode))
+			reduceFeeAmtToZero(feeCode, "B_TSDC", "STANDARD");
+		return;
+	}
+
+	feeSum = sumASITColumn(tdtSDCTable, "Line Item Fee Total");
+	if (feeSum > 0) {
+		//if (feeExists(feeCode) && (feeSum != getFeeQty(feeCode))) {
+		//	voidRemoveFees(feeCode);
+		//}
+		updateFee(feeCode, "B_TSDC", "STANDARD", feeSum, "N");
+	} else {
+		if (feeExists(feeCode))
+			//voidRemoveFees(feeCode);
+			reduceFeeAmtToZero(feeCode, "B_TSDC", "STANDARD");
+
+	}
+}
+
+///// SDC fees for Quick Calc
+
+
+/// Parks soHi SDC fees
+
