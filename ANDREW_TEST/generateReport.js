b9fd1dfbbcf47c6346084496b1f8c4e31a498770
fragrom <fragrom@gmail.com>
2017-11-08 15:23:46 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/generateReport.js
@@ -0,0 +1,32 @@
+function generateReport(aaReportName, parameters, rModule) {
+	var reportName = aaReportName;
+
+	report = aa.reportManager.getReportInfoModelByName(reportName);
+	report = report.getOutput();
+
+	report.setModule(rModule);
+	report.setCapId(capId);
+
+	report.setReportParameters(parameters);
+
+	var permit = aa.reportManager.hasPermission(reportName, currentUserID);
+
+	if (permit.getOutput().booleanValue()) {
+		var reportResult = aa.reportManager.getReportResult(report);
+
+		if (reportResult) {
+			reportResult = reportResult.getOutput();
+			var reportFile = aa.reportManager.storeReportToDisk(reportResult);
+			logMessage("Report Result: " + reportResult);
+			reportFile = reportFile.getOutput();
+			return reportFile
+		} else {
+			logMessage("Unable to run report: " + reportName + " for Admin" + systemUserObj);
+			return false;
+		}
+	} else {
+		logMessage("No permission to report: " + reportName + " for Admin" + systemUserObj);
+		return false;
+	}
+}
+
