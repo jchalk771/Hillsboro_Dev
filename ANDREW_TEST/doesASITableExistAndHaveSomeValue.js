de1f883014723c0f07dc268fecc1a1bdbcd550d3
fragrom <fragrom@gmail.com>
2017-11-07 16:52:31 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/doesASITableExistAndHaveSomeValue.js
@@ -0,0 +1,16 @@
+doesASITableExistAndHaveSomeValue(tName)
+ {
+	tempASIT = loadASITable(tName);
+	if (tempASIT == undefined || tempASIT == null)
+		return false;
+	for (var ea in tempASIT) {
+		var row = tempASIT[ea];
+		for (var col in row) {
+			fv = "" + row[col].fieldValue;
+			if (fv != undefined && fv != null && fv != "") {
+				return true;
+			}
+		}
+	}
+	return false;
+}
