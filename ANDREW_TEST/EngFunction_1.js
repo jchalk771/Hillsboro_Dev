eb960682f718f2783367b4683dbae2e6756e7e6c
fragrom <fragrom@gmail.com>
2017-11-07 16:50:17 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/EngFunction_1.js
@@ -0,0 +1,25 @@
+EngFunction_1()
+ {
+
+	var t1;
+	var t2;
+	var t3;
+	var t4;
+	var x1;
+
+	t1 = AInfo["Financial Assurance"];
+	t2 = AInfo["Certificate of Insurance"];
+	t3 = AInfo["Workmens Comp"];
+	t4 = AInfo["Contracts"];
+
+	if (((t1 == "Yes") || (t1 == "NA")) &&
+		((t2 == "Yes") || (t2 == "NA")) &&
+		((t3 == "Yes") || (t3 == "NA")) &&
+		((t4 == "Yes") || (t4 == "NA"))) {
+		return "Success";
+	} else {
+
+		return "Endless Failure";
+	}
+
+}
