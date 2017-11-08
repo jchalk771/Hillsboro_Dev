3eecfa54a9912c38c2af5fd4d533273c228dcc04
fragrom <fragrom@gmail.com>
2017-11-08 15:22:30 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getRequiredInspTypeList.js
@@ -0,0 +1,28 @@
+function getRequiredInspTypeList() {
+
+	var itemCap = capId;
+
+	if (arguments.length == 1)
+		itemCap = arguments[0];
+
+	var retArray = new Array();
+	var inspTypeListResult = aa.inspection.getInspectionListForSchedule(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
+
+	if (inspTypeListResult.getSuccess()) {
+		var inspTypeList = inspTypeListResult.getOutput();
+		if (inspTypeList == null)
+			return retArray;
+		for (var i in inspTypeList) {
+			var thisInspType = inspTypeList[i];
+
+			if (thisInspType.getRequiredInspection().equals("Y")) {
+				logDebug(thisInspType.getDispType() + " is required");
+				retArray.push(thisInspType.getDispType());
+			}
+		}
+	} else {
+		logDebug("Error getting inspection list " + inspTypeListResult.getErrorMessage());
+	}
+	return retArray;
+}
+
