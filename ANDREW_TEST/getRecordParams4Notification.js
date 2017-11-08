6ad7bd4ee0bd797dcdc21d4a9657908ad17334e6
fragrom <fragrom@gmail.com>
2017-11-08 15:23:46 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getRecordParams4Notification.js
@@ -0,0 +1,16 @@
+function getRecordParams4Notification(params) {
+	// pass in a hashtable and it will add the additional parameters to the table
+
+	addParameter(params, "$$altID$$", capIDString);
+	addParameter(params, "$$capName$$", capName);
+	addParameter(params, "$$capStatus$$", capStatus);
+	addParameter(params, "$$fileDate$$", fileDate);
+	addParameter(params, "$$workDesc$$", workDescGet(capId));
+	addParameter(params, "$$balanceDue$$", "$" + parseFloat(balanceDue).toFixed(2));
+	addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias());
+	//if (wfComment != null) {
+	//addParameter(params, "$$wfComment$$", wfComment);
+	//}
+	return params;
+}
+
