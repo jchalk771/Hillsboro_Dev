3ff9e9f685b574d05dd0faa50f5e7bcac96e8dc5
fragrom <fragrom@gmail.com>
2017-11-07 16:52:35 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/parksSDCcheck.js
@@ -0,0 +1,12 @@
+parksSDCcheck()
+ {
+	var J;
+	var H;
+	J = "false";
+	J = doesASITableExistAndHaveSomeValue("PARKS SDC INFORMATION");
+	if (J == "true") {
+		updateParksFee();
+	} else {
+		return;
+	}
+}
