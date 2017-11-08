ffb9f23486d54745ed7db6857c7dbd7461d2bde1
fragrom <fragrom@gmail.com>
2017-11-07 16:52:41 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/assessDeferralFees.js
@@ -0,0 +1,25 @@
+assessDeferralFees(feeCode, feeSch) {
+	//removeFee(feeCode, "STANDARD");
+	feeCount = 0;
+	tables = ["LARGE PROJ DEFERRED SUBMITTAL", "PLAN REVIEW DEFERRED SUBMITTAL"];
+	for (eachTable in tables) {
+		tObj = loadASITable(tables[eachTable]);
+		if (tObj == undefined || tObj == null || tObj.length == 0)
+			continue;
+		for (eachRow in tObj) {
+			thisRow = tObj[eachRow];
+			dflit = thisRow["Deferral Fee Line Item Total"].fieldValue;
+			if (thisRow["Deferral Payment Status"].fieldValue == "Invoiced" || thisRow["Deferral Payment Status"].fieldValue == "Invoice") {
+				if (dflit != null && dflit != "")
+					//updateFee(feeCode, feeSch, "STANDARD", parseFloat(dflit), "N");
+					addFee(feeCode, feeSch, "STANDARD", parseFloat(dflit), "N");
+				//if (thisRow["Deferral Review Status"].fieldValue == "Approved") {
+				//		feeCount++;
+			}
+		}
+	}
+	//if (feeCount > 0) {
+	//	updateFee(feeCode, feeSch, "STANDARD", feeCount, "N");
+	//}
+}
+
