b1eb2e1f6c66a9bad0dc3b417435fd44394e06cf
fragrom <fragrom@gmail.com>
2017-11-07 16:51:20 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/eleFeeExists.js
@@ -0,0 +1,18 @@
+eleFeeExists()
+ {
+	feesch = "B_ELE";
+
+	var feeResult = aa.fee.getFeeItems(capId);
+	if (feeResult.getSuccess()) {
+		var feeObjArr = feeResult.getOutput();
+	} else {
+		logDebug("**ERROR: getting fee items: " + capContResult.getErrorMessage());
+		return false
+	}
+
+	for (ff in feeObjArr)
+		if (feesch.equals(feeObjArr[ff].getF4FeeItemModel().getFeeSchudle()))
+			return true;
+
+	return false;
+}
