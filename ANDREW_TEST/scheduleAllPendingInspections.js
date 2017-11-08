6663af4551b1ec41c11c8f2e62d0bea3f8f87129
fragrom <fragrom@gmail.com>
2017-11-07 16:51:23 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/scheduleAllPendingInspections.js
@@ -0,0 +1,38 @@
+scheduleAllPendingInspections()
+ {
+	var schedTomorrow = (new Date().getHours() >= 6)
+
+	if (arguments.length == 1) {
+		itemCap = arguments[0];
+	} else {
+		var itemCap = capId;
+	}
+	var inspResultObj = aa.inspection.getInspections(itemCap);
+	if (inspResultObj.getSuccess()) {
+		var inspList = inspResultObj.getOutput();
+		for (xx in inspList) {
+			inspObj = inspList[xx];
+			if (inspObj.getInspectionType().indexOf("FINAL") == -1 && inspObj.getDocumentDescription().equals("Insp Pending") && inspObj.getAuditStatus().equals("A")) {
+				inspModel = inspObj.getInspection();
+				actModel = inspModel.getActivity();
+				if (actModel.getRequiredInspection().equals("Y")) {
+					daysAhead = lookup("ScheduleInspDaysAhead", inspObj.getInspectionType().trim())
+						var nDays = 0;
+					if (daysAhead != null && daysAhead != "undefined") {
+						nDays = parseInt(daysAhead, 10);
+					} else {
+						if (schedTomorrow)
+							nDays++;
+					}
+					newDate = dateAdd(null, nDays);
+					newAADate = aa.util.parseDate(newDate);
+					actModel.setActivityDate(newAADate);
+
+					inspModel.setActivity(actModel);
+					systemUserObj = aa.person.getUser("ADMIN").getOutput();
+					aa.inspection.scheduleInspection(inspModel, systemUserObj);
+				}
+			}
+		}
+	}
+}
