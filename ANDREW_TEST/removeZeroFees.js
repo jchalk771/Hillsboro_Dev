ca446b1660e37198e96d0afd3f94b70b3d6b701d
fragrom <fragrom@gmail.com>
2017-11-07 16:51:50 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/removeZeroFees.js
@@ -0,0 +1,10 @@
+removeZeroFees()
+ {
+	var feeArr = loadFees();
+	for (x in feeArr) {
+		thisFee = feeArr[x];
+		if (thisFee.status == "NEW" && thisFee.amount == 0) {
+			removeFee(thisFee.code, "STANDARD");
+		}
+	}
+}
