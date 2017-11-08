4f6446f3d78eb935b0f3650f0d4af1a6506455f6
fragrom <fragrom@gmail.com>
2017-11-07 16:51:29 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/updateParksBank.js
@@ -0,0 +1,20 @@
+updateParksBank()
+ {
+	tObj = loadASITable("PARKS SDC INFORMATION");
+	unitTotalAdd = sumASITColumn(tObj, "Line Item Unit Total", "INCLUDE", "Type of Change", "Add Unit");
+	unitTotalRemove = sumASITColumn(tObj, "Line Item Unit Total", "INCLUDE", "Type of Change", "Remove Unit");
+	unitTotal = roundNumber((unitTotalAdd + unitTotalRemove), 2);
+	if (unitTotal > 0) {
+		addToTLIDBankField("Existing Parks Units", unitTotal);
+	}
+	if (unitTotal < 0) {
+		addToTLIDBankField("Existing Parks Units", unitTotal);
+		addToTLIDBankField("Parks Credit Units", -unitTotal);
+	}
+
+	creditUnits = roundNumber(sumASITColumn(tObj, "Line Item Unit Total", "INCLUDE", "Type of Change", "Use Unit Credit"), 2);
+	if (creditUnits != 0) {
+		logDebug("Subtracting " + creditUnits + " to bank field Parks Credit Units");
+		addToTLIDBankField("Parks Credit Units", creditUnits);
+	}
+}
