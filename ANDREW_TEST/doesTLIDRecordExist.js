e50a46c78c8308f7de7f6143bed83485f750319c
fragrom <fragrom@gmail.com>
2017-11-07 16:51:52 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/doesTLIDRecordExist.js
@@ -0,0 +1,16 @@
+doesTLIDRecordExist()
+ {
+
+	retValue = false;
+	pNum = getPrimaryParcelNum();
+	if (pNum != null) {
+		tlidResult = aa.cap.getCapID(pNum);
+		if (tlidResult.getSuccess()) {
+			tlidCapId = tlidResult.getOutput();
+			if (tlidCapId != null) {
+				return true;
+			}
+		}
+	}
+	return retValue;
+}
