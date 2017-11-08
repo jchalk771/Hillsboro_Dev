219721cabf98ad879d8b819b87f91a5d9bd8fb63
fragrom <fragrom@gmail.com>
2017-11-07 16:52:15 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/getSchoolFeeConstants.js
@@ -0,0 +1,21 @@
+getSchoolFeeConstants(Period, schoolDistrictNumber, useType)
+ {
+	var calcFactor = "";
+	calcFactor = (Period.toString() + schoolDistrictNumber.toString() + useType.toString());
+	logDebug("calcFactor code is: " + calcFactor);
+
+	var constArray = new Array();
+	constArray = lookup("School_Fee_Constant_Lookup", calcFactor).split("|"); //lookup returns a string
+
+
+	logDebug("constArray[cap]is: " + constArray[0]);
+	logDebug("constArray[rate]is: " + constArray[1]);
+
+	calcCap = parseFloat(constArray[0]);
+	calcRate = parseFloat(constArray[1]);
+
+	return {
+		rate : calcRate,
+		cap : calcCap
+	}; //sending back calRate and calcCap in a JSON Array
+}
