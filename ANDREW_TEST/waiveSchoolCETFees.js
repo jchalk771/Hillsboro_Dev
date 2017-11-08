b821cf492a707d386f7cf147fbd82e2b93b5ef27
fragrom <fragrom@gmail.com>
2017-11-07 16:52:14 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/waiveSchoolCETFees.js
@@ -0,0 +1,26 @@
+waiveSchoolCETFees(schoolDistrict)
+ {
+
+	if (schoolDistrict == "HILLSBORO" || schoolDistrict == "Hillsboro") {
+		if (feeExists(lookup("School_Fee_Code_Lookup", "HillsCityCombined")))
+			updateSGFee(lookup
+
+				("School_Fee_Code_Lookup", "HillsCityWaiver"), "B_STR", "CETHILL");
+		if (feeExists(lookup("School_Fee_Code_Lookup", "HillsSchCombined")))
+			updateSGFee(lookup
+
+				("School_Fee_Code_Lookup", "HillsSchWaiver"), "B_STR", "CETHILLSCH");
+	}
+
+	if (schoolDistrict == "BEAVERTON" || schoolDistrict == "Beaverton") {
+		if (feeExists(lookup("School_Fee_Code_Lookup", "BeavCityCombined")))
+			updateSGFee(lookup
+
+				("School_Fee_Code_Lookup", "BeavCityWaiver"), "B_STR", "CETBEAV");
+		if (feeExists(lookup("School_Fee_Code_Lookup", "BeavSchCombined")))
+			updateSGFee(lookup
+
+				("School_Fee_Code_Lookup", "BeavSchWaiver"), "B_STR", "CETBEAVSCH");
+	}
+
+}
