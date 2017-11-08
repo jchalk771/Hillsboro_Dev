8507768f148196d8f1c4fcbe1d5eb06a7fb14524
fragrom <fragrom@gmail.com>
2017-11-08 15:23:27 -0800

    Uploaded by Andrew

---
+++ ANDREW_TEST/doesScheduledInspectionExist.js
@@ -0,0 +1,18 @@
+function doesScheduledInspectionExist(iType) {
+	var itemCap = capId;
+	if (arguments.length == 3) {
+		itemCap = arguments[2];
+	} // use cap ID specified in args
+
+	var inspResultObj = aa.inspection.getInspections(itemCap);
+	if (inspResultObj.getSuccess()) {
+		var inspList = inspResultObj.getOutput();
+		for (xx in inspList) {
+			if (inspList[xx].getInspectionType().equals(iType) && inspList[xx].getInspectionStatus().equals("Scheduled")) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
