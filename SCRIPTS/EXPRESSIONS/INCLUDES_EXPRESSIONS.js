2d6ae471c9d22b81906a44d106e24b59ff882eae
Jeanne Chalk <jchalk@accela.com>
2018-02-01 15:20:24 -0500

    After Payment expression work

--- SCRIPTS/EXPRESSIONS/INCLUDES_EXPRESSIONS.js
+++ SCRIPTS/EXPRESSIONS/INCLUDES_EXPRESSIONS.js
@@ -1,44 +1,46 @@
-/*===========================================
+/*===========================================================================================================
+|
+|	Title : INCLUDES_EXPRESSIONS
+|
+|	Functional Area : Accela Product Management and Delivery Solutions
+|
+|	Description : This INCLUDES script contains utility functions to support advanced expression development
+|
+============================================================================================================ */
+//logDebug("====IMPORTING INCLUDES_EXPRESSIONS====");
 
-Title : INCLUDES_EXPRESSIONS
-
-Functional Area : Accela Product Management and Delivery Solutions
-
-Description : This INCLUDES script contains utility functions to support advanced expression development
-
-=========================================== */
-logDebug("====IMPORTING INCLUDES_EXPRESSIONS====");
- 
 function getMessageStyle() {
+
 	var cssStyle = "<style>.infoMsg, .successMsg, .warningMsg, .errorMsg, .validationMsg {	\
-				margin: 10px 0px; \
-				padding:12px;	\
-			}	\
-			.infoMsg {	\
-				color: #00529B;	\
-				background-color: #BDE5F8;	\
-			}	\
-			.successMsg {	\
-				color: #4F8A10;	\
-				background-color: #DFF2BF;	\
-			}	\
-			.warningMsg {	\
-				color: #9F6000;	\
-				background-color: #FEEFB3;	\
-			}	\
-			.errorMsg {	\
-				color: #D8000C;	\
-				background-color: #FFBABA;	\
-			}	\
-			.infoMsg i, .successMsg i, .warningMsg i, .errorMsg i {	\
-		    margin:10px 22px;	\
-		    font-size:2em;	\
-		    vertical-align:middle;	\
-			}</style>";
+										margin: 10px 0px; \
+										padding:12px;	\
+									}	\
+									.infoMsg {	\
+										color: #00529B;	\
+										background-color: #BDE5F8;	\
+									}	\
+									.successMsg {	\
+										color: #4F8A10;	\
+										background-color: #DFF2BF;	\
+									}	\
+									.warningMsg {	\
+										color: #9F6000;	\
+										background-color: #FEEFB3;	\
+									}	\
+									.errorMsg {	\
+										color: #D8000C;	\
+										background-color: #FFBABA;	\
+									}	\
+									.infoMsg i, .successMsg i, .warningMsg i, .errorMsg i {	\
+								    margin:10px 22px;	\
+								    font-size:2em;	\
+								    vertical-align:middle;	\
+									}</style>";
 	return cssStyle;
 }
 
 function logDebug(dstr) {
+
 	var vLevel = "info";
 	if (arguments.length == 2) {
 		vLevel = arguments[1]; // debug level
@@ -57,6 +59,7 @@ function logDebug(dstr) {
 }
 
 function notice(dstr) {
+
 	var vLevel = "info";
 	if (arguments.length == 2) {
 		vLevel = arguments[1];
@@ -77,14 +80,17 @@ function notice(dstr) {
 }
 
 function lookup(stdChoice, stdValue) {
+
 	var strControl;
 	var bizDomScriptResult = aa.bizDomain.getBizDomainByValue(stdChoice, stdValue);
 
 	if (bizDomScriptResult.getSuccess()) {
+
 		var bizDomScriptObj = bizDomScriptResult.getOutput();
 		strControl = "" + bizDomScriptObj.getDescription(); // had to do this or it bombs.  who knows why?
 		logDebug("lookup(" + stdChoice + "," + stdValue + ") = " + strControl);
 	} else {
+
 		logDebug("lookup(" + stdChoice + "," + stdValue + ") does not exist");
 	}
 	return strControl;
