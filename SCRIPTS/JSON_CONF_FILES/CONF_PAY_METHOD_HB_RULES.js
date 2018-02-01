de0466456b98c38596681997015e673bb40aba72
Jeanne Chalk <jchalk@accela.com>
2018-02-01 15:22:31 -0500

    Updated with new structure

--- SCRIPTS/JSON_CONF_FILES/CONF_PAY_METHOD_HB_RULES.js
+++ SCRIPTS/JSON_CONF_FILES/CONF_PAY_METHOD_HB_RULES.js
@@ -3,272 +3,320 @@
     "reference": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Check": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Contract": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Credit Card": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Dept Account": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "EPC - CC": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "EPC - Check": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Money Order": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Online Payment": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Other": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Trust Account": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Web": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Void": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "EPC-Void": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "Re-Dist": {
     "reference": {
       "required": true,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   },
   "DEFAULT": {
     "reference": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Reference #/CC Auth Code"
     },
     "comment": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Comments"
     },
     "receivedType": {
       "required": false,
       "hidden": false,
-      "readOnly": false
+      "readOnly": false,
+      "fieldAlias": "Received"
     }
   }
 }
